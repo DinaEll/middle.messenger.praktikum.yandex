d90e0afd6b8d52736d262677eadf5ca3cda47e95
Dina Pupina <NPupina@beeline.ru>
2024-06-12 19:18:45 +0300

    Update front pages

--- src/pages/changeData-page/index.js
+++ src/pages/changeData-page/index.js
@@ -1,2 +1,20 @@
 import './changeData-page.scss';
 export { default as ChangeDataPage } from './changeData-page.hbs?raw';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const avatarInput = document.querySelector('.changeData-page__avatar-input');
+    const avatarImage = document.querySelector('.chat-page__profile-icon');
+
+    avatarInput.addEventListener('change', handleAvatarUpload);
+
+    function handleAvatarUpload(event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            avatarImage.src = e.target.result;
+        }
+
+        reader.readAsDataURL(file);
+    }
+});
---
+++ src/pages/user-page/index.js
@@ -0,0 +1,2 @@
+import './user-page.scss';
+export { default as UserPage } from './user-page.hbs?raw';
