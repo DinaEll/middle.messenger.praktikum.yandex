82a6df446043a2882309fb9c78f60d9bdf3d58f5
Dina Pupina <NPupina@beeline.ru>
2024-07-18 20:24:03 +0300

    правка стилей 1

--- src/components/chat-list/chat-list.ts
+++ src/components/chat-list/chat-list.ts
@@ -1,41 +1,101 @@
-import {IProps,Block} from "../../utils/Block";
+import {IProps, Block} from "../../core/block";
 import {IChat} from "../../modelsInterface/IChat";
-import {ChatItem} from "../index";
-import {IChatItemProps} from "../chat-item/chat-item";
+import {IUser} from "../../modelsInterface/IUser";
+import modalController from "../../core/modal-controller";
+import ModalPrompt from "../2/modal-prompt";
+import {createChat} from "../../services/chat";
+import {updateChats, setStateCurrentChat} from "../../services/app";
+import {StoreEvents} from "../../core/store";
+
 
 interface IChatListProps extends IProps {
-   list:IChat[]
+    list: IChat[],
+    currentUser: IUser | null,
+    showModalAddChat: () => void,
+    setCurrentChat: (chat: string) => void
 }
 
 export class ChatList extends Block {
     constructor(props: IChatListProps) {
-        super(props);
+
+    props.currentUser = window.store.getState().user||null;
+    props.list = window.store.getState().chats || [];
+    props.showModalAddChat = () => {
+      modalController.addModal((new ModalPrompt({
+        caption: 'Add Chat',
+        labelText: 'Title Chat',
+        okText: 'Add Chat',
+        ref: "modal",
+        okClick: (result: string) => {
+          createChat(result)
+            .then(async () => await updateChats())
+            .catch((error)=>console.warn(error));
+        },
+      })) as unknown as Block);
+      modalController.openModal();
+    }
+    props.setCurrentChat = (id: string) => {
+      const chat = this.props.list.find(item => item.id === Number(id)) || null;
+      setStateCurrentChat(chat).then(() => {
+        this.setProps(this.props)
+      })
     }
 
-   getChats(list:IChat[]):string{
-        if(!list||list.length===0)return '';
-        return list.map(chat=>{
-            const chatBlock=new ChatItem({chat:chat} as IChatItemProps)
-            return(`
-                ${chatBlock.renderForList()}
-            `)
-        }).join('')
-    }
-    protected render(): string {
-        const {list } = this._props as IChatListProps;
 
-        return (`
+    super({
+      ...props
+    })
+
+    window.store.on(StoreEvents.Updated, () => {
+      this.props.currentUser = window.store.getState().user||null;
+      this.props.list = window.store.getState().chats || [];
+      this.setProps(this.props);
+    });
+  }
+
+  public get props() {
+    return this._props as IChatListProps;
+  }
+
+  getChats(list: IChat[]): string {
+    if (!list || list.length === 0) return `<li class="chat-list__chats-empty">{{{Button caption="Add chat" type='link' onClick=showModalAddChat }}}</li>`;
+    return list.map(chat => {
+      return (`  {{{ChatItem
+                    onClick=setCurrentChat
+                    id='${chat.id} '
+                    title='${chat.title} '
+                    avatar='${chat.avatar} '
+                    unread_count='${chat.unread_count > 0 ? String(chat.unread_count) : ''}'
+                    last_message_content='${chat.last_message ? chat.last_message.content : 'no messages'} '
+                    last_message_time='${chat.last_message ? chat.last_message.time : ''}' }}} `)
+    }).join('')
+  }
+
+  protected render(): string {
+    const {list, currentUser} = this.props;
+    if (!currentUser) return `
+            <div class="container container-center">
+                 {{{Loader }}}
+            </div>`
+
+    //if (!list || list.length === 0) return 'Add chat';
+    return (`
             <div class="chat-list">
-                <nav class="chat-list-header">
-                    {{{Link caption="Профиль" page="pageProfile"  linkIcon=true }}}
+                <nav class="chat-list__header">
+                ${currentUser && `{{{ Avatar imageUrl='${currentUser.avatar || ''}' size='sm' }}}`}
+                {{{Button caption="New Chat" type='link' onClick=showModalAddChat }}}
+                 {{{Link caption="Profile" href="/settings"  linkIcon=true }}}
                 </nav>
-                <div class="chat-list-search">
+<!--                <div class="chat-list__search">
                     {{{ InputSearch }}}
-                </div>
-                <ul class="chat-list-chats">
+                </div>-->
+                <ul class="chat-list__chats">
                     ${this.getChats(list)}
                 </ul>
+                <div class="chat-list__footer">
+
+                </div>
             </div>
         `)
-    }
+  }
 }
--- src/components/form-profile/form-profile.ts
+++ src/components/form-profile/form-profile.ts
@@ -1,55 +1,78 @@
-import {IProps,Block} from "../../utils/Block";
+import {IProps,Block} from "../../core/block";
 import {IUser} from "../../modelsInterface/IUser";
 import {ALL_VALIDATE_FIELDS, IValidateType} from "../../modelsInterface/IValidateType";
+import {logOut} from "../../services/auth";
 
-interface IFormProfileProps extends IProps{
-    user:IUser,
-    withButton:boolean,
+interface IFormProfileProps extends IProps {
+    user?: IUser | null,
+    withButton: boolean,
     children: string,
-    buttonText:string,
-    buttonPage:string,
-    buttonCancelPage:string,
-    onClickOkButton: (event:Event) => void,
-    validate:IValidateType,
+    buttonText: string,
+    onClickOkButton: (event: Event) => void,
+    onChangeStateForm: (isEditPassword: boolean, isEditProfile: boolean) => void,
+    onLogOut: (event: Event) => void,
+    onCancel: (event: Event) => void,
+    onClickLink: (event: Event) => void,
+    validate: IValidateType,
 
 }
-export class FormProfile extends Block {
-    constructor(props:IFormProfileProps) {
-        props.validate= ALL_VALIDATE_FIELDS;
 
+export class FormProfile extends Block {
+
+    constructor(props: IFormProfileProps) {
+
+        props.validate = ALL_VALIDATE_FIELDS;
+        props.onLogOut = (event: Event) => {
+            event.preventDefault();
+            logOut().catch((error) => console.warn(error))
+        };
+        props.onClickLink = (event: Event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            const id=(event.target as Element).id;
+            if(id==='edit_password')this.props.onChangeStateForm(true,false);
+            if(id==='edit_profile')this.props.onChangeStateForm(false,true);
+        };
+
+        props.user = window.store.getState().user;
         super(props);
     }
 
-    protected render(): string {
-        const {user,withButton=false,children='',buttonText='',
-            buttonCancelPage=''}=this._props as IFormProfileProps;
-        const {avatar,first_name,second_name}=user;
+    public get props() {
+        return this._props as IFormProfileProps;
+    }
 
-        return(`
+    protected render(): string {
+        const {user, withButton = false, children = '', buttonText = ''} = this.props;
+        if (!user) return '';
+        const {avatar = '', first_name, second_name} = user;
+        return (`
 
         <div class="profile">
-            <div class="profile-avatar">
-                {{{ Avatar image=${avatar} isLoadAvatar=true }}}
-                <h2 class="profile-avatar-name">${first_name} ${second_name}</h2>
+            <div class="profile__avatar">
+                {{{ Avatar imageUrl='${avatar || ''}' isLoadAvatar=true }}}
+                <h2 class="profile__avatar__name">${first_name} ${second_name}</h2>
             </div>
             ${user ?
-          `<div class = "profile-main" >
+            `<div class = "profile__main" >
                 ${children}
             </div>` : ''
         }
              ${withButton ?
-          `<div class="profile__button">
-                    {{{ Button caption="${buttonText}" onClick=onClickOkButton }}}
+            `<div class="profile__button">
+                    {{{ Button caption="${buttonText}" onClick=onClickOkButton isSubmit=true}}}
                 </div>` :
-          `<div class="profile-buttons">
-                    {{{Link caption="Изменить профиль пользователя" page="changeData" type='success' linkLine=true  }}}
-                    {{{Link caption="Изменить пароль" page="changePassword" type='success' linkLine=true  }}}
-                    {{{Link caption="Выход" page="startPage" type='danger' }}}
-                </div>`}
+          `<div class="profile__buttons">
+                    {{{Link caption="Изменить профиль пользователя" href='' type='success' linkLine=true onClick=onClickLink id='edit_profile'}}}
+                    {{{Link caption="Изменить пароль" href='' type='success' linkLine=true onClick=onClickLink id='edit_password'}}}
+                    {{{Button caption="Выход" onClick=onLogOut type='link' }}}
+                </div>`
+        }
+             <div class="block-cancel">
+                {{{ Button type="cancel"  onClick=onCancel}}}
             </div>
-            <div class="block-cancel">
-                {{{ Button type="cancel" page="${buttonCancelPage}" }}}
-            </div>
+        </div>
+
         `)
     }
 }
