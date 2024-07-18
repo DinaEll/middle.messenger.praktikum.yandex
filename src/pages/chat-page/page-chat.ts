3df682e4fa18a5cd46826b3298cf1b9acb70ff6a
Dina Pupina <NPupina@beeline.ru>
2024-07-18 18:48:20 +0300

    обновление части страниц 2

---
+++ src/pages/chat-page/page-chat.ts
@@ -0,0 +1,24 @@
+import {IProps, Block} from "../../core/block";
+
+export class PageChat extends Block {
+
+    constructor() {
+        const props: IProps = {
+            events: {}
+        }
+        super(props);
+    }
+
+    protected render(): string {
+        return (`
+           <div class="chat-page">
+                <div class="chat-page-left">
+                    {{{ ChatList }}}
+                </div>
+                <div class="chat-page-main">
+                    {{{ MessageList }}}
+                </div>
+            </div>
+        `)
+    }
+}
