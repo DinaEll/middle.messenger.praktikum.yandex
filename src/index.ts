a5a4d6d6b7bb6cce7eb9689ef01fdb48923a4028
Dina Pupina <NPupina@beeline.ru>
2024-07-18 20:54:59 +0300

    фикс багов 3

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,46 @@
 import './styles/index.scss';
 import * as Components from './components';
 import * as Pages from './pages';
-import {registerComponent} from "./utils/registerComponents";
-import {Block} from "./utils/Block";
+import {registerComponent} from "./core/register-components";
+import Router from "./core/router";
+import {BASE_URLS} from "./config";
+import {initialStateApp} from "./services/app";
+import {IAppState} from "./modelsInterface/IAppState";
+import {Store} from "./core/store";
+
+Object.entries(Components).forEach(
+    ([componentName, component]) => registerComponent(componentName, component)
+)
 
 
-const allComponents = {
-  'Button': Components.Button,
-  'Avatar': Components.Avatar,
-  'Badge': Components.Badge,
-  'Input': Components.Input,
-  'InputShort': Components.InputShort,
-  'InputWide': Components.InputWide,
-  'InputSearch': Components.InputSearch,
-  'Link': Components.Link,
-  'Error': Components.Error,
-  'ChatItem': Components.ChatItem,
-  'ChatList': Components.ChatList,
-  'Message': Components.Message,
-  'MessageList': Components.MessageList,
-  'Loader': Components.Loader,
-  'Modal': Components.Modal,
-  'FormAuth': Components.FormAuth,
-  'FormProfile': Components.FormProfile,
+declare global {
+    interface Window {
+        store: Store<IAppState>;
+    }
+
+    type Nullable<T> = T | null;
+
 }
-const pages: { [index: string]: { component: unknown } } = {
-  "startPage": {component: Pages.StartPage},
-  "login": {component: Pages.LoginPage},
-  "registrationPage": {component: Pages.RegistrationPage},
-  "pageProfile": {component: Pages.PageProfile},
-  "changeData": {component: Pages.ChangeData},
-  "changePassword": {component: Pages.ChangePassword},
-  "errorInternalServer": {component: Pages.ErrorInternalServer},
-  "errorNotFound": {component: Pages.ErrorNotFound},
-  "chatPage": {component: Pages.ChatPage},
-};
 
-Object.entries(allComponents).forEach(([name, component]) => {
-  registerComponent(name, component);
-});
-
-const navigate = (page: string) => {
-  const app = document.getElementById('app');
-  const Component = pages[page].component as unknown as typeof Block;
-  const component = new Component({events: {}});
-  const htmlElement = component.getContent();
-  if (!app?.firstElementChild) app?.append(document.createElement('div'));
-  if (htmlElement)
-    app?.firstElementChild?.replaceWith(htmlElement);
+const initState: IAppState = {
+    error: null,
+    user: undefined,
+    currentChat: null,
+    chats: [],
 }
-document.addEventListener('DOMContentLoaded', () => navigate('startPage'));
-document.addEventListener('click', (e: Event) => {
-  if (!e) return;
-  if (!e.target) return;
-  const page = (<HTMLDivElement>e.target).getAttribute('page');
-  if (page) {
-    navigate(page);
-    e.preventDefault();
-    e.stopImmediatePropagation();
-  }
-});
+
+window.store = new Store<IAppState>(initState);
+
+const router = new Router(".app");
+initialStateApp();
+
+
+router/*.use(BASE_URLS['page-default'], Pages.PageChat)*/
+    .use(BASE_URLS['page-all-components'], Pages.AllComponentsPage)
+    .use(BASE_URLS['page-login'], Pages.LoginPage)
+    .use(BASE_URLS['page-sign-up'], Pages.PageRegistration)
+    .use(BASE_URLS['page-profile'], Pages.PageProfile)
+    .use(BASE_URLS['page-404'], Pages.Page404)
+    .use(BASE_URLS['page-500'], Pages.Page500)
+    .use(BASE_URLS['page-chat'], Pages.PageChat)
+    .start();
