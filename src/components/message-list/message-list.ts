e4227c33d32a52e6d372408ec05364aede3bdffb
Dina Pupina <NPupina@beeline.ru>
2024-07-18 22:32:14 +0300

    наводим красоту

--- src/components/message-list/message-list.ts
+++ src/components/message-list/message-list.ts
@@ -1,11 +1,11 @@
-import {IProps, Block} from "../../data/block.ts";
-import {IChatMessage} from "../../modelsInterface/IChatMessage.ts";
-import {IUser} from "../../modelsInterface/IUser.ts";
-import {Message} from "../index.ts";
-import {IMessageProps} from "../message/message.ts";
-import {IChat} from "../../modelsInterface/IChat.ts";
-import {StoreEvents} from "../../data/store.ts";
-import {getUserName} from "../../utils/user.utils.ts";
+import {IProps, Block} from "../../data/block";
+import {IChatMessage} from "../../modelsInterface/IChatMessage";
+import {IUser} from "../../modelsInterface/IUser";
+import {Message} from "../index";
+import {IMessageProps} from "../message/message";
+import {IChat} from "../../modelsInterface/IChat";
+import {StoreEvents} from "../../data/store";
+import {getUserName} from "../../utils/user.utils";
 
 interface IMessageListProps extends IProps {
     messageList: IChatMessage[];
